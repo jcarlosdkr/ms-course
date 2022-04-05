664a76446c5bd33171bb7b6ca0ba9755bdb60fdf
jcarlosdkr <jcarlos.dkr@gmail.com>
2022-04-05 13:49:59 -0500

    adding services

---
+++ config-service/src/main/java/com/example/store/config/ConfigServiceApplication.java
@@ -0,0 +1,14 @@
+package com.example.store.config;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@EnableConfigServer
+@SpringBootApplication
+public class ConfigServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConfigServiceApplication.class, args);
+	}
+
+}
